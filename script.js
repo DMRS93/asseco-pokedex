7e9f35803a8cf703851b68a0e644000fbb76da87
Diogo Sousa <97111591+DMRS93@users.noreply.github.com>
2022-05-03 17:34:25 +0100

    DiogoS

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-const pokemonListUrl = "https://pokeapi.co/api/v2/pokemon?limit=150";
+'use strict';
+
+const pokemonListUrl = 'https://pokeapi.co/api/v2/pokemon?limit=150';
+
+const pokeCont = document.querySelector('.container');
+
+const renderPoke = function (data, className = '') {
+  const html = `
+    <article class="country ${className}">
+      
+      <div class="country__data">
+        <h3 class="country__name">${data.results}</h3>
+    `;
+  pokeCont.insertAdjacentHTML('beforeend', html);
+};
+
+const Show150 = async function () {
+  const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=150');
+
+  const data = await res.json();
+  if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+  console.log(res);
+  console.log(data);
+  renderPoke(data);
+};
+Show150();
